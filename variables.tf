0a6809c7874c65f566edd69426a6dd83c695de29
Abhishek Tiwari <abhishek.tiwari@nashtechglobal.com>
2026-01-08 14:16:04 +0530

    refactor: uncomment AMI and EC2 instance resources in Terraform files

--- variables.tf
+++ variables.tf
@@ -2,6 +2,11 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-# variable "instance_type" {
-#   default = "t3.micro"
+# variable "instance_ami" {
+#   description = "Amazon Linux 2 AMI for ap-south-1"
+#   default     = "ami-00ca570c1b6d79f36"
 # }
+
+variable "instance_type" {
+  default = "t3.micro"
+}
